940c317ce62d352f8b519665d6003cf57a5d1e5c
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-08-20 00:08:50 +0200

    code update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more about the React
+          Learn React
         </a>
       </header>
     </div>
