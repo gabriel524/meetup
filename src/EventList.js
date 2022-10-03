ebc908f29636210ac4ddd4f635a674719949c022
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-03 21:58:04 +0200

    new file added to project folder

---
+++ src/EventList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+    render() {
+        return (
+            <ul className = "EventList">
+
+            </ul>
+            
+        );
+    }
+}
+
+export default EventList;
