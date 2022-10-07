e9c1ecc7b921e1d3fe4e6953b33dcf9cd9d97172
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-07 19:33:26 +0200

    new file added to project folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import EventList from "./tests/EventList";
+import EventList from "./EventList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <EventList />
-
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
--- src/EventList.js
+++ src/EventList.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import Event from "./Event";
 
-export class EventList extends Component {
+class EventList extends Component {
   render() {
-    const { events } = this.props;
-    return (
-      <ul className="EventList">
-        {events.map((event) => (
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        ))}
-      </ul>
-    );
+      return (
+          <ul className="EventList">
+              
+          </ul>
+      );
+          
   }
 }
 
