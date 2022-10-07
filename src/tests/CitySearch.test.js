2c271d03522e4567d69480951de2a2ac1434431c
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-07 23:38:48 +0200

    update to code file

---
+++ src/tests/CitySearch.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CitySearch from "../CitySearch";
+
+describe("<CitySearch /> component", () => {
+  test("render text input", () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find(".city")).toHaveLength(1);
+  });
+  test("renders a list of suggestions", () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
+  });
+  test("renders text input correctly", () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    const query = CitySearchWrapper.state("query");
+    expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
+  });
+});
