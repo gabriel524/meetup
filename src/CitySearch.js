1af976e25f349c80d7b525df3a2e292877fede0d
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-11 15:05:52 +0200

    update to code file

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,15 +3,33 @@ import React, { Component } from "react";
 class CitySearch extends Component {
   state = {
     query: "",
+    suggestions: [],
   };
-  render() {
 
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  };
+
+  render() {
     return (
       <div className="CitySearch">
-        <input type="text"
+        <input
+          type="text"
           className="city"
-          value={this.state.query} />
-        <ul className="suggestions"></ul>
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+        <ul className="suggestions">
+          <ul className="suggestions">
+            {this.state.suggestions.map((suggestion) => (
+              <li key={suggestion}>{suggestion}</li>
+            ))}
+            <li key="all">
+              <b>See all cities</b>
+            </li>
+          </ul>
+        </ul>
       </div>
     );
   }
