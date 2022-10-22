559616efaf0480380e12e35f16ade85842a9ed43
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-22 14:34:46 +0200

    file update for improvement

---
+++ src/CitySearch.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+export class CitySearch extends Component {
+  state = {
+    query: "",
+    suggestions: [],
+    showSuggestions: undefined,
+  };
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({ query: value, suggestions });
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+      showSuggestions: false,
+    });
+    this.props.updateEvents(suggestion);
+  };
+
+  render() {
+    return (
+      <div className="CitySearch">
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+        <ul
+          className="suggestions"
+        >
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+          <li key="all">
+            <b>See all cities</b>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
