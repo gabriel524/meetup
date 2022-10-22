5b779b74a3236daa7a8d4d8e228788197d2a6502
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-22 18:07:05 +0200

    file update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+  };
+
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} />
+        <EventList events={this.state.events} />
+        <NumberOfEvents />
+      </div>
+    );
   }
 }
 
 export default App;
-
--- src/tests/CitySearch.test.js
+++ src/tests/CitySearch.test.js
@@ -26,20 +26,20 @@ describe("<CitySearch /> component", () => {
     });
     expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
   });
-  
+
   test("render a list of suggestions", () => {
     expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
   });
 
-   test("selecting a suggestion should change query state", () => {
-     CitySearchWrapper.setState({
-       query: "Berlin",
-     });
-     const suggestions = CitySearchWrapper.state("suggestions");
-     CitySearchWrapper.find(".suggestions li").at(0).simulate("click");
-     expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
-   });
-  
+  test("selecting a suggestion should change query state", () => {
+    CitySearchWrapper.setState({
+      query: "Berlin",
+    });
+    const suggestions = CitySearchWrapper.state("suggestions");
+    CitySearchWrapper.find(".suggestions li").at(0).simulate("click");
+    expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+  });
+
   test("render list of suggestions correctly", () => {
     const locations = extractLocations(mockData);
     CitySearchWrapper.setState({ suggestions: locations });
@@ -66,6 +66,4 @@ describe("<CitySearch /> component", () => {
     const query = CitySearchWrapper.state("query");
     expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
   });
-
-  
 });
