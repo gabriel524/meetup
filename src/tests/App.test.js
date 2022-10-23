b46c7a3fe7c29262431f462f6564e1140c3bc2b5
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-23 21:15:05 +0200

    update

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -25,10 +25,10 @@ describe("<App /> component", () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 
-  describe('<App /> integration', () => {
+  describe("<App /> integration", () => {
     test('App passes "events" state as a prop to EventList', () => {
       const AppWrapper = mount(<App />);
-      const AppEventsState = AppWrapper.state('events');
+      const AppEventsState = AppWrapper.state("events");
       expect(AppEventsState).not.toEqual(undefined);
       expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
       AppWrapper.unmount();
