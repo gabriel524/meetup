d713a56cdc8df401cd76bf535af09088fc1b6f3e
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-24 00:09:59 +0200

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { mockData } from "./mock-data";
-import axios from "axios";
 import NProgress from "nprogress";
+import axios from "axios";
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -8,7 +8,23 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-export const checkToken = async (accessToken) => {
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    "https://nbwr0ujhgg.execute-api.eu-central-1.amazonaws.com/dev/api/token/" +
+      encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
+const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -18,6 +34,20 @@ export const checkToken = async (accessToken) => {
   return result;
 };
 
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
 export const getEvents = async () => {
   NProgress.start();
 
@@ -25,7 +55,6 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
-
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
@@ -36,7 +65,10 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = `https://8stm0qydxd.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
+    const url =
+      "https://8stm0qydxd.execute-api.us-east-1.amazonaws.com/dev/api/get-events" +
+      "/" +
+      token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
@@ -45,24 +77,6 @@ export const getEvents = async () => {
     }
     NProgress.done();
     return result.data.events;
-  }
-};
-
-const getToken = async (code) => {
-  try {
-    const encodeCode = encodeURIComponent(code);
-
-    const response = await fetch(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${encodeCode}`
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-    return access_token;
-  } catch (error) {
-    error.json();
   }
 };
 
@@ -84,18 +98,4 @@ export const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
-};
-
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
 };
