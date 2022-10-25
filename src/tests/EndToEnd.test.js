e76dba9bfc7ac9f4a3816883e494449a6533835c
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-25 15:18:53 +0200

    file update

--- src/tests/EndToEnd.test.js
+++ src/tests/EndToEnd.test.js
@@ -6,9 +6,9 @@ describe("show/hide an event details", () => {
   let page;
   beforeAll(async () => {
     browser = await puppeteer.launch({
-     /* headless: false,
+      headless: false,
       slowMo: 250, // slow down by 250ms
-      ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors*/
+      ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
     });
     page = await browser.newPage();
     await page.goto("http://localhost:3000/");
