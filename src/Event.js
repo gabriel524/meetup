de11e3d79196d741c000f0a72f8405f6e4413cb4
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-10-31 21:03:53 +0100

    code update in event.js

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div className="events"></div>;
+    return (
+      <div className="events">
+        <button className="details-btn"></button>
+      </div>
+    );
   }
 }
 export default Event;
