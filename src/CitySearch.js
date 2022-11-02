deb6443157b7b623bb0760a9595e564597278f2c
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-02 23:00:10 +0100

    update to code file

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -13,18 +13,28 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      suggestions: [],
       showSuggestions: false,
+      infoText: "",
     });
-
     this.props.updateEvents(suggestion);
   };
 
