9d4eab258433d843b1b0abcf48170db0ac2f1dc8
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-07 20:32:00 +0100

    file update

--- src/tests/NumberOfEvents.test.js
+++ src/tests/NumberOfEvents.test.js
@@ -7,28 +7,29 @@ describe("<NumberOfEvents /> component", () => {
   beforeAll(() => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
-
-  test("render number input", () => {
-    expect(NumberOfEventsWrapper.find(".events-num-input")).toHaveLength(1);
-  });
-
-  test("render default number in the input is 32", () => {
-    expect(NumberOfEventsWrapper.find(".events-num-input").prop("value")).toBe(
-      32
-    );
-  });
-
-  test("render change the number of events in input field by changing state", () => {
-    NumberOfEventsWrapper.setState({
-      numOfEvents: 32,
+    test("render number input", () => {
+      expect(NumberOfEventsWrapper.find(".events-num-input")).toHaveLength(1);
     });
-    const eventObject = { target: { value: 6 } }; 
-    NumberOfEventsWrapper.find(".events-num-input").simulate(
-      "change",
-      eventObject
-    );
-    expect(NumberOfEventsWrapper.state("numOfEvents")).toBe(32);
-  });
+
+    test("render default number in the input is 32", () => {
+      expect(
+        NumberOfEventsWrapper.find(".events-num-input").prop("value")
+      ).toBe(32);
+    });
+
+    test("render change the number of events in input field by changing state", () => {
+      NumberOfEventsWrapper.setState({
+        numOfEvents: 32,
+      }); 
+      const eventObject = { target: { value: 6 } };
+      NumberOfEventsWrapper.find(".events-num-input").simulate(
+        "change",
+        eventObject
+      );
+      console.log("Abraham");
+      
+      expect(NumberOfEventsWrapper.state("numOfEvents")).toBe(32);
+    });
 });
 
 
