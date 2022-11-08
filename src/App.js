aba8e141765bd583c81ed83a2722a59bd974545c
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-08 12:46:53 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -3,33 +3,23 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import "./nprogress.css";
-import Row from "react-bootstrap/Row";
-import WelcomeScreen from "./WelcomeScreen";
-import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import './nprogress.css';
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
-    showWelcomeScreen: undefined,
   };
 
-  async componentDidMount() {
+  componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -57,36 +47,24 @@ class App extends Component {
   };
 
   render() {
-    if (this.state.showWelcomeScreen === undefined)
-      return <div className="App" />;
-    const { numberOfEvents } = this.state;
+    const {numberOfEvents} = this.state;
     return (
       <div className="App">
-        <div className="title-wrapper">
-          <h1>Meet App</h1>
-          <h4>Please choose your nearest city</h4>
-        </div>
-        <div className="data-wrapper">
-          <CitySearch
-            locations={this.state.locations}
-            updateEvents={this.updateEvents}
-          />
-          <EventList events={this.state.events} />
-          <NumberOfEvents
-            numberOfEvents={numberOfEvents}
-            updateEvents={this.updateEvents}
-          />
-        </div>
-        <Row className="events-wrapper"></Row>
-        <WelcomeScreen
-          showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => {
-            getAccessToken();
-          }}
+        <h1>Meet App</h1>
+        <h4>Please choose your nearest city</h4>
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
         />
+        <NumberOfEvents
+          numberOfEvents={numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
+        <EventList events={this.state.events} />
       </div>
     );
   }
 }
+
 
 export default App;
