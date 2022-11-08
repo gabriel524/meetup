25e93d182535b18fb3a89c0585803ab1b208a296
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-08 13:46:45 +0100

    file change

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import './nprogress.css';
-import { getEvents, extractLocations } from "./api";
-
+import "./nprogress.css";
+import Row from "react-bootstrap/Row";
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 class App extends Component {
   state = {
     events: [],
@@ -15,13 +16,23 @@ class App extends Component {
     showWelcomeScreen: undefined,
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events,
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
   }
 
   updateEvents = (location, maxNumEvents) => {
