8f34506fc3bbc94ee3a9118dc096b53e82d8c3e7
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-08 15:24:08 +0100

    update

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -6,8 +6,8 @@ function WelcomeScreen(props) {
     <div className="WelcomeScreen">
       <video autoPlay loop muted id="video">
         <source
-          src="https://www.pexels.com/video/people-in-a-conference-room-for-a-business-meeting-3205624/"
-          type="video.mp4"
+          src="https://www.pexels.com/video/people-in-a-conference-room-for-a-business-meeting-261-large.mp4"
+          type="video/mp4"
         />
       </video>
       <div className="overlay"></div>
