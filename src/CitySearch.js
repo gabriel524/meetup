308f1f196383505a0811b64e93df4d623fc26fd4
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-10 21:17:04 +0100

    update for further improvement

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -44,6 +44,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <p>Search for a City</p>
         <div className="infoAlert">
           <InfoAlert text={this.state.infoText} />
         </div>
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -27,7 +27,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="event_number">
-        <label className="number-of-events">Show Number of Events: </label>
+        <div div className="number-of-events">Show Number of Events: </div>
         <input
           type="number"
           id="events-num-input"
