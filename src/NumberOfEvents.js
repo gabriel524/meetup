370e4b0909bd33b23010101e4f1b3f5bb074b6fa
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-11 17:54:22 +0100

    file change

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,40 +3,27 @@ import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
-    allEvents: [],
     numberOfEvents: 32,
-    locationSelected: "all",
     infoText: "",
   };
 
   handleInputChanged = (event) => {
-    console.log(event.target.value);
     let inputValue = event.target.value;
-    if (isNaN(inputValue)) {
-      return "";
-    } else {
-      parseInt(inputValue);
-    }
-    const allEvents = this.state.allEvents;
-    const { locationSelected } = this.state;
-    const locationEvents =
-      locationSelected === "all"
-        ? allEvents
-        : allEvents.filter((event) => event.location === locationSelected);
-    if (inputValue > 32 || inputValue <= 0) {
+    if (inputValue >= 33 || inputValue <= 0) {
       this.setState({
         numberOfEvents: inputValue,
-        events: locationEvents.slice(0, inputValue),
-        infoText: "Please choose a number between 1 and 32",
+        infoText: "Please enter a number between 1 - 32.",
       });
     } else {
       this.setState({
-        numberOfEvents: inputValue,
-        events: locationEvents.slice(0, inputValue),
+        numberOfEvents: event.target.value,
         infoText: " ",
       });
     }
+
+    //this.props.updateEvents(undefined, inputValue);
   };
+
   render() {
     return (
       <div className="event_number">
