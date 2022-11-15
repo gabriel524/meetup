e4018105b2c881de09129eb6559393fac4a33663
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-15 14:33:27 +0100

    file code update

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -43,7 +43,7 @@ class CitySearch extends Component {
 
   render() {
     return (
-      <div className="CitySearch data-wrapper">
+      <div className="CitySearch">
         <p>Search for a City</p>
         <div className="infoAlert">
           <InfoAlert text={this.state.infoText} />
