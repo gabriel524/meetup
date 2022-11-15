64c72cce2532d697e83b8c94f3b6ce35347f4165
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-15 14:38:05 +0100

    update to file

--- src/App.js
+++ src/App.js
@@ -103,25 +103,27 @@ class App extends Component {
         </div>
 
         <div className="data-wrapper">
-          <h4>Events in each city</h4>
-          <EventGenre events={events} />
-          <ResponsiveContainer height={400}>
-            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 10 }}>
-              <CartesianGrid />
-              <XAxis type="category" dataKey="city" name="city" />
-              <YAxis
-                allowDecimals={false}
-                type="number"
-                dataKey="number"
-                name="number of events"
-              />
-              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-              <Scatter data={this.getData()} fill="#ff7300" />
-            </ScatterChart>
-          </ResponsiveContainer>
+            <h4>Events in each city</h4>
+            <EventGenre events={events} />
+            <ResponsiveContainer height={400}>
+              <ScatterChart
+                margin={{ top: 20, right: 20, bottom: 20, left: 10 }}
+              >
+                <CartesianGrid />
+                <XAxis type="category" dataKey="city" name="city" />
+                <YAxis
+                  allowDecimals={false}
+                  type="number"
+                  dataKey="number"
+                  name="number of events"
+                />
+                <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+                <Scatter data={this.getData()} fill="#ff7300" />
+              </ScatterChart>
+            </ResponsiveContainer>
+      
+          <EventList events={events} />
         </div>
-
-        <EventList events={events} />
         <Row className="events-wrapper"></Row>
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
