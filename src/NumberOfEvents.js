15ec55598d11d2a324e81f0e0014de4228d58a6c
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2023-01-24 19:54:58 +0100

    file update

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -59,66 +59,6 @@ class NumberOfEvents extends Component {
 export default NumberOfEvents;
 
 
-/*handleInputChanged = (event) => {
-    console.log(event);
-    let inputValue = event.target.value;
-
-    if (inputValue > 1 || inputValue > 32) {
-      this.setState({
-        infoText: "Please select number from 1 to 32",
-      });
-    } else {
-      this.props.updateEvents(null, inputValue);
-      this.setState({
-        numberOfEvents: inputValue,
-        infoText: "",
-      });
-    }
-  };*/
-
-  /*handleInputChanged = (event) => {
-    console.log(event);
-    let inputValue = event.target.value;
-    if (inputValue < 1 || inputValue > 32) {
-      this.setState({
-        numberOfEvents: inputValue,
-        infoText: "Please enter a number between 1 - 32.",
-      });
-    } else {
-      this.setState({
-        numberOfEvents: event.target.value,
-        infoText: " ",
-      });
-    }
-
-    this.props.updateEvents(inputValue);
-  };
-  
-  
-
-
-   handleInputChanged = (e)=>{
-    const inputValue=e.target.value;
-    
-    if (inputValue<1 || inputValue>32) {
-      this.setState({
-        infoText: 'Please select number from 1 to 32'
-      })
-      
-    } else {
-      this.props.updateEvents(null, inputValue);
-      this.setState({
-        eventCount: inputValue,
-        infoText:''
-      })
-    }
-  }
-  
-  
-  
-  
-  */
-
 
 
 
